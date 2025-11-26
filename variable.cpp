2a9d4d2bb41339891e03aafb02c636269f6ad8e9
AJ Gebara <ajgebara@gmail.com>
2025-11-25 19:50:12 -0600

    succesfully compiles

--- variable.cpp
+++ variable.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-  std::cout<<"Hello World";
+  int num = 10;
+  double numDeci = 23.2;
+  float floatNum = 67.7777777;
+  char letter = 'A';
+  string helloWorld = "Hello World!";
+  bool isThisTrue = true;
+  
+  cout << "num: " << num << endl;
+  cout << "numDeci: " << numDeci << endl;
+  cout << "floatNum: " << floatNum << endl;
+  cout << "letter: " << letter << endl;
+  cout << "helloWorld: " << helloWorld << endl;
+  cout << "isThisTrue: " << isThisTrue << endl;
+  
   return 0;
 }
